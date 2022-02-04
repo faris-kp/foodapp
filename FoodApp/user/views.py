04751f3846c7d2c1d1801c747ae8666688950fd2
unknown <fariskp46820@gmail.com>
2022-02-04 12:03:35 +0530

    signup front end reset

--- FoodApp/user/views.py
+++ FoodApp/user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View
-
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
 
 class Index(View):
     template_name = 'user/index.html'
@@ -20,7 +22,13 @@ class SignUp(View):
         return render(request,self.template_name)
 
     def post(self,request):
-        pass
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
+        return JsonResponse({'username_valid': True})
 
 
 class SignIn(View):
