3f72fb120c1ccfc480c72feb7378e43bd458ab5a
unknown <fariskp46820@gmail.com>
2022-02-06 13:36:35 +0530

    emailvalidation

--- FoodApp/static/js/signup.js
+++ FoodApp/static/js/signup.js
@@ -1,22 +1,45 @@
 const usernameField=document.querySelector("#usernameField");
+const emailField = document.querySelector("#emailField");
 
 
+emailField.addEventListener("keyup", (e) =>{
+    console.log("enter emailfieled")
+    const emailVal = e.target.value;
+    console.log("email",emailVal)
+    emailField.classList.remove("fm-inavlid")
+    emailField.classList.add("fm-valid")
+    $('.email_error').hide();
+    if (emailVal.length > 0) {
+        fetch("signup", {
+          body: JSON.stringify({ email: emailVal }),
+          method: "POST",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+        console.log("data", data);
+        if (data.email_error) {
+            console.log("enter emailfielederror")
+            emailField.classList.remove("fm-valid")
+            emailField.classList.add("fm-inavlid")
+            $('.email_error').show();
+            $(`.email_error`).html(`<ul class="errorlist"><li>${data.email_error}</li></ul>`);
+        }
+    });
+     }
+    });
 usernameField.addEventListener("keyup", (e) => {
     console.log('4444',44444);
     const usernameVal = e.target.value;
+    console.log("user",usernameVal)
     usernameField.classList.remove("fm-inavlid")
     usernameField.classList.add("fm-valid")
     $('.image_error').hide();
-
-    
-        
     if (usernameVal.length > 0)
     {
     fetch("signup", { 
         body: JSON.stringify({ username: usernameVal }),
         method:"POST",
-
-    })
+     })
     .then((res) => res.json())
     .then((data) => {
         console.log("data",data)
--- FoodApp/user/views.py
+++ FoodApp/user/views.py
@@ -4,7 +4,9 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from  validate_email import validate_email
 import json
+import sys
 
 class Index(View):
     template_name = 'user/index.html'
@@ -25,13 +27,40 @@ class SignUp(View):
         return render(request,self.template_name)
 
     def post(self,request):
-        data = json.loads(request.body)
-        username = data['username']
-        if not str(username).isalnum():
-            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
-        if User.objects.filter(username=username).exists():
-            return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
-        return JsonResponse({'username_valid': True})
+        try:
+            username=''
+            email=''
+            data = json.loads(request.body)
+            print("data",data)
+            if len(data) > 3:
+                pass # TODO signUp
+            if 'username' in data:
+                username = data['username']
+                if not str(username).isalnum():
+                    print("enterd")
+                    return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+                elif User.objects.filter(username=username).exists():
+                    return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
+                else:
+                    return JsonResponse({'username_valid': True})
+                    
+               
+            else:
+                email = data['email']
+                print(email)
+                if not validate_email(email):
+                    print("enterdemail")
+                    return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+                elif User.objects.filter(email=email).exists():
+                    return JsonResponse({'email_error': 'sorry email in use,choose another one '}, status=409)
+                else:
+                    return JsonResponse({'email_valid': True})
+            
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(e,str(exc_tb.tb_lineno))
+
+        
 
 
 class SignIn(View):
